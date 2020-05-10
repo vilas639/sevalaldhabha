bb8e039a0590007d90de5a3b78eaae7da44fd207
vilas639 <vilasjdhv639@gmail.com>
2020-05-10 20:27:06 +0530

    added list details
    added fonts, list category , list details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:sevalaldhabha/categories_screen.dart';
+import 'package:sevalaldhabha/screens/categoriesmeal_screen.dart';
+import 'package:sevalaldhabha/screens/meal_detail_screen.dart';
+import 'package:sevalaldhabha/screens/tabs_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,9 +13,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sevalal Dhabha',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            body2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            title: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
       ),
-      home: CategoriesScreen(),
+    //  home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+      //  '/' : (ctx) => CategoriesScreen(),(),
+        '/' : (ctx) => TabsScreen(),
+        Categoriesmeal.routeName : (ctx) => Categoriesmeal(),
+         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+
+      }
+        
+      
 
     );
   }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:sevalaldhabha/widgets/category_item.dart';
 
-import './dummy_data.dart';
-import './category_item.dart';
+import '../dummy_data.dart';
+
+
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sevalal Dhabha'),
-      ),
+     
       body: GridView(
+         padding: const EdgeInsets.all(25),
+
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
+                    catData.id,
                     catData.title,
                     catData.color,
+                    
                   ),
             )
             .toList(),
