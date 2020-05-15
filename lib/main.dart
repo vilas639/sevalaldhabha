6e1cd86daf4d2b5d613fb1ddb30722d93a80fd2b
vilas639 <vilasjdhv639@gmail.com>
2020-05-15 17:42:40 +0530

    added favourite screen
    add in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:sevalaldhabha/dummy_data.dart';
+import 'package:sevalaldhabha/models/meal.dart';
 import 'package:sevalaldhabha/screens/categoriesmeal_screen.dart';
+import 'package:sevalaldhabha/screens/filters_screen.dart';
 import 'package:sevalaldhabha/screens/meal_detail_screen.dart';
 import 'package:sevalaldhabha/screens/tabs_screen.dart';
 
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+
+    void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+   bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +96,13 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', // default is '/'
       routes: {
       //  '/' : (ctx) => CategoriesScreen(),(),
-        '/' : (ctx) => TabsScreen(),
-        Categoriesmeal.routeName : (ctx) => Categoriesmeal(),
-         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        '/' : (ctx) => TabsScreen(_favoriteMeals),
+        Categoriesmeal.routeName : (ctx) => Categoriesmeal(_availableMeals),
+         MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
+          FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
+
+      
+
 
       }
         
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,46 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:sevalaldhabha/models/meal.dart';
+import 'package:sevalaldhabha/screens/favorites_screen.dart';
+import 'package:sevalaldhabha/widgets/main_drawer.dart';
+
+import 'categories_screen.dart';
 
 
-import './categories_screen.dart';
-import 'favorites_screen.dart';
 
 class TabsScreen extends StatefulWidget {
+
+  final List<Meal> favoriteMeals;
+
+  TabsScreen(this.favoriteMeals);
+
+
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  @override
+
+List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 0;
+
+
+
+     @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': FavoritesScreen(widget.favoriteMeals),
+        'title': 'Your Favorite',
+      },
+    ];
+    super.initState();
+  }
+
+void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+
+   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
